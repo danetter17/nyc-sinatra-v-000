57ec50578259dad785fa612a8acebe4e57cebc62
Daniel Etter <etter.dt@gmail.com>
2019-01-15 22:26:37 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,4 +3,8 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
 end

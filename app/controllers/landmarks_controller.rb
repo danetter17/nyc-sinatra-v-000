18ba59da7985b54002d6c1cc1910753d6f133a01
Daniel Etter <etter.dt@gmail.com>
2019-01-15 23:03:17 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,6 +11,6 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     @landmark = Landmark.create(params[:landmark])
     @landmark.save
-    redirect to "landmarks/#{@landmark.id}"
+    redirect to "/landmarks/#{@landmark.id}"
   end
 end

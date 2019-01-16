6b7767c6092ee387df6fb7fc7838552ee75b19f5
Daniel Etter <etter.dt@gmail.com>
2019-01-16 00:30:16 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -22,7 +22,7 @@ class LandmarksController < ApplicationController
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find_by_id(params[:id])
     erb :"/landmarks/edit"
-    binding.pry
+    #binding.pry
   end
 
   patch '/landmarks/:id' do
